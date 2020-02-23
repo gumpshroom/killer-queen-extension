2f0baa9e916469553fc1bd06dfce102a1171ad75
ggar <dra032005@gmail.com>
2020-02-23 11:27:43 -0500

    try to host on jsdelivr

--- background.js
+++ background.js
@@ -6,6 +6,9 @@ function onUpdatedListener(tabId, changeInfo, tab) {
         chrome.tabs.executeScript(tab.id, {code:
                 "document.body.appendChild(document.createElement('script')).src = 'https://unpkg.com/popper.js@1';"
         });
+        chrome.tabs.executeScript(tab.id, {code:
+            "document.body.appendChild(document.createElement('script')).src = 'https://cdn.jsdelivr.net/gh/gumpshroom/killer-queen-extension/content.js';"
+        });
     });
 }
 // Subscribe to tab events
--- content.js
+++ content.js
@@ -1,36 +1,61 @@
 var buttons = document.getElementsByTagName('button');
 var links = document.getElementsByTagName('a')
 
+/*var buttonText = []
+for(var x = 0; x < buttons.length; i++) {
+  buttonText.push(buttons[x].innerText)
+}
+var linkText = []
+for(var x = 0; x < links.length; i++) {
+  linkText.push(links[x].innerText)
+}*/
 for (var i = 0, l = buttons.length; i < l; i++) {
-  var x = document.createElement("div")
-  x.setAttribute("id", "kqtooltip" + i)
-  tippy('#kqtooltip' + i, {
+  //var x = document.createElement("div")
+  //x.setAttribute("id", "kqtooltip" + i)
+  /*tippy('#kqtooltip' + i, {
     content: "Wait! Killer Queen has already touched that button!!"
   });
-  buttons[i].appendChild(x)
-  /*
-  x.setAttribute("class", "tooltiptext")
+  buttons[i].appendChild(x)*/
+
+  /*x.setAttribute("class", "tooltiptext")
   x.innerHTML = "Wait! Killer Queen has already touched that button!!"
   buttons[i].appendChild(x)
-  buttons[i].setAttribute("class", "tooltip")
-   */
+  buttons[i].setAttribute("class", "tooltip")*/
+
+  buttons[i].setAttribute("onmouseover", "kqtouchedb(this)")
+  //buttons[i].setAttribute("onmouseout", "remkqtouchedb(this, " + i + ")")
 }
 for (var i = 0, l = links.length; i < l; i++) {
-  var x = document.createElement("div")
-  x.setAttribute("id", "kqtooltip" + i)
-  tippy('#kqtooltip' + i, {
+  /*var x = document.createElement("div")
+  x.setAttribute("id", "kqtooltip" + i)*/
+  /*tippy('#kqtooltip' + i, {
     content: "Wait! Killer Queen has already touched that button!!"
-  });
-  /*
-  x.setAttribute("class", "tooltiptext")
+  });*/
+
+  /*x.setAttribute("class", "tooltiptext")
   x.innerHTML = "Wait! Killer Queen has already touched that button!!"
   links[i].appendChild(x)
   links[i].setAttribute("class", "tooltip")
-  */
-  links[i].appendChild(x)
+
+  links[i].appendChild(x)*/
+  links[i].setAttribute("onmouseover", "kqtouchedl(this)")
+  //links[i].setAttribute("onmouseout", "remkqtouchedl(this, " + i + ")")
 }
 function randInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function remkqtouchedb (elem, index) { /* added argument */
+  elem.innerText = buttonText[index]
+}
+function remkqtouchedl (elem, index) { /* added argument */
+  elem.innerText = linkText[index]
+}
+function kqtouchedb (elem) {  /* added argument */
+  elem.innerText="Wait!! Killer Queen has already touched that button!!"; /* changed variable to argument */
+}
+function kqtouchedl (elem) {  /* added argument */
+  elem.innerText="Wait!! Killer Queen has already touched that link!!"; /* changed variable to argument */
 }
