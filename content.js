cc06ac54ffaac0bcffc72d0746d836ea2e13ed82
ggar <dra032005@gmail.com>
2020-02-23 18:06:52 -0500

    remove junk

--- content.js
+++ content.js
@@ -1,39 +1,7 @@
-var buttons = document.getElementsByTagName('button');
 var links = document.getElementsByTagName('a')
 var PERCENT = 0.5
-/*var buttonText = []
-for(var x = 0; x < buttons.length; i++) {
-  buttonText.push(buttons[x].innerText)
-}
-var linkText = []
-for(var x = 0; x < links.length; i++) {
-  linkText.push(links[x].innerText)
-}*/
-//for (var i = 0, l = buttons.length; i < l; i++) {
-  //var x = document.createElement("div")
-  //x.setAttribute("id", "kqtooltip" + i)
-  /*tippy('#kqtooltip' + i, {
-    content: "Wait! Killer Queen has already touched that button!!"
-  });
-  buttons[i].appendChild(x)*/
-
-  /*x.setAttribute("class", "tooltiptext")
-  x.innerHTML = "Wait! Killer Queen has already touched that button!!"
-  buttons[i].appendChild(x)
-  buttons[i].setAttribute("class", "tooltip")*/
-  //if(buttons[i].innerText === "") {
-   // buttons[i].setAttribute("prevkqtext", buttons[i].innerHTML)
-    //buttons[i].setAttribute("onmouseover", "this.firstChild.innerText = \"Wait!! Killer Queen has already touched that button!!\"")
-    //buttons[i].setAttribute("onmouseout", "this.innerHTML = \"" + buttons[i].getAttribute("prevkqtext") + "\"")
-  //} else {
-    //buttons[i].setAttribute("prevkqtext", buttons[i].innerText)
-    //buttons[i].setAttribute("onmouseover", "this.innerText = \"Wait!! Killer Queen has already touched that button!!\"")
-    //buttons[i].setAttribute("onmouseout", "this.innerText = \"" + buttons[i].getAttribute("prevkqtext") + "\"")
-  //}
-//}
 var processed = 0;
 while(processed < Math.floor(links.length * PERCENT)) {
-//for (var i = 0, l = Math.floor(links.length * PERCENT); i < l; i++) {
   var i;
   var foundnewlink = false;
   while(!foundnewlink) {
@@ -47,19 +15,6 @@ while(processed < Math.floor(links.length * PERCENT)) {
     }
   }
   if(links[i].getAttribute("prevkqtext") == null) {
-    /*var x = document.createElement("div")
-    x.setAttribute("id", "kqtooltip" + i)*/
-    /*tippy('#kqtooltip' + i, {
-      content: "Wait! Killer Queen has already touched that button!!"
-    });*/
-
-    /*x.setAttribute("class", "tooltiptext")
-    x.innerHTML = "Wait! Killer Queen has already touched that button!!"
-    links[i].appendChild(x)
-    links[i].setAttribute("class", "tooltip")
-
-    links[i].appendChild(x)*/
-    //if(links[i].innerText === "") {
     links[i].setAttribute("prevkqtext", links[i].innerHTML)
     links[i].onmouseover = function () {
       for (var x = 0; x < this.childNodes.length; x++) {
@@ -86,30 +41,11 @@ while(processed < Math.floor(links.length * PERCENT)) {
     }
     processed ++;
   }
-
-  //} else {
-  //  links[i].setAttribute("prevkqtext", links[i].innerText)
- //   links[i].setAttribute("onmouseover", "this.innerText = \"Wait!! Killer Queen has already touched that link!!\"")
- //   links[i].setAttribute("onmouseout", "this.innerText = \"" + links[i].getAttribute("prevkqtext") + "\"")
-  //}
 }
 function randInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function remkqtouchedb (elem, index) { /* added argument */
-  elem.innerText = buttonText[index]
-}
-function remkqtouchedl (elem, index) { /* added argument */
-  elem.innerText = linkText[index]
-}
-function kqtouchedb (elem) {  /* added argument */
-  elem.innerText="Wait!! Killer Queen has already touched that button!!"; /* changed variable to argument */
-}
-function kqtouchedl (elem) {  /* added argument */
-  elem.innerText="Wait!! Killer Queen has already touched that link!!"; /* changed variable to argument */
 }
 (function() {
   var Bomb, Explosion, Particle, targetTime, vendor, w, _i, _len, _ref,
